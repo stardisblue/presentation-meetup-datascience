a586741917aafd534f011f5c7cee94f11a74d5c2
Fati Chen <fati.chen@comwatt.com>
2023-12-12 14:07:51 +0100

    update e2e

--- demo/test/test_get_measures_of_date_for_devices.py
+++ demo/test/test_get_measures_of_date_for_devices.py
@@ -1,6 +1,7 @@
 from demo.adapters.local_parquet_reader import LocalParquetReader
 from demo.adapters.in_memory_writer import InMemoryWriter
 from demo.get_measures_of_date_for_devices import get_measures_of_date_for_devices
+
 from test.helpers import assertDataFrameEqual
 
 
@@ -8,9 +9,6 @@ def test_get_measures_of_date(spark):
     datestamp = "2023-09-11"
     devices = LocalParquetReader(spark, "data/devices.parquet")
     measures = LocalParquetReader(spark, "data/measures.parquet")
-
-    assert devices().count() == 3
-    assert measures().count() == 144
 
     device_measures = InMemoryWriter()
     device_measures(get_measures_of_date_for_devices(datestamp, devices, measures))
